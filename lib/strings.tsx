6931a7c3a8e998765ff14a5cd7d2715e76a851f7
AFIF ZILANI <afifzilani4566@gmail.com>
2026-01-15 00:28:12 +0600

    refactor: clean up TooltipCreator function for improved readability

--- lib/strings.tsx
+++ lib/strings.tsx
@@ -1,27 +1,39 @@
 import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
 } from "@/components/ui/tooltip";
 
 export function TooltipCreator({ text }: { text?: string }) {
-  const shortId = (id: string) => id.slice(0, 14) + "...";
-  if (text) {
-    if (text.length < 14){
-      return text
+    const shortId = (id: string) => id.slice(0, 14) + "...";
+    if (text) {
+        if (text.length < 14) {
+            return text;
+        }
+        return (
+            <TooltipProvider>
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        <div className="text-muted-foreground cursor-pointer">
+                            {shortId(text)}
+                        </div>
+                    </TooltipTrigger>
+                    <TooltipContent>{text}</TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
+        );
     }
-    return (
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <div className="text-muted-foreground cursor-pointer">
-              {shortId(text)}
-            </div>
-          </TooltipTrigger>
-          <TooltipContent>{text}</TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
-    );
-  }
+}
+
+export function formatCurrencyInBDT(amount: number) {
+    const number = Number(amount) || 0;
+
+    const formatted = number
+        .toFixed(2)
+        .replace(/\.00$/, "") // remove .00
+        .replace(/(\.\d)0$/, "$1") // remove trailing zero like .50 → .5
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    return `৳${formatted}`;
 }
