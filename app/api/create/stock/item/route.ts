01e3202f211c663671e33d0f859ac7bfff359fb0
AFIF ZILANI <afifzilani4566@gmail.com>
2026-01-15 00:29:03 +0600

    fix: update supplier ID mapping in actor search results

--- app/api/create/stock/item/route.ts
+++ app/api/create/stock/item/route.ts
@@ -1,14 +1,28 @@
+import { Prisma } from "@/app/generated/prisma/client";
 import { errorResponse, response } from "@/lib/apiResponse";
+import prisma from "@/lib/prisma";
 import { addStockItemSchema } from "@/schemas/item.schem";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
     try {
         const body = await req.json();
+        console.log(body);
+
         const data = addStockItemSchema.parse(body);
-        return response({
-            message: "",
+
+        await prisma.item.create({
+            data: {
+                name: data.name,
+                category: data.category,
+                unit: data.unit,
+                meta_data: data.isMetaDataAvailable
+                    ? data.metaData || {}
+                    : Prisma.JsonNull, // <-- use Prisma.JsonNull instead of null
+            },
         });
+
+        return response({ message: "Success" });
     } catch (error) {
         return errorResponse(error);
     }
