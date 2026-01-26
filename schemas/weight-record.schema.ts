5dcf70beae422514a99639c813be407594e55fdf
AFIF ZILANI <afifzilani4566@gmail.com>
2026-01-26 14:36:54 +0600

    feat:enhance user experience and reduce user error. update backend logic

--- schemas/weight-record.schema.ts
+++ schemas/weight-record.schema.ts
@@ -1,24 +1,10 @@
-import z from "zod";
+import { z } from "zod";
+import { decimalNumber } from "./helper";
 
 export const addWeightRecordSchema = z.object({
-    batchId: z.string().uuid("Please select a valid batch"),
-    houseId: z.number().positive("Please select a house"),
-    avgWeightInGrams: z.preprocess(
-        (val) => {
-            if (typeof val === "string" && val !== "") {
-                return Number(val);
-            }
-            return val;
-        },
-        z
-            .number()
-            .finite()
-            .positive()
-            .refine(
-                (v) => Number.isInteger(v * 100),
-                "Maximum 2 decimal places allowed"
-            )
-    ),
+    houseId: z.string().uuid(),
+    occurredAt: z.coerce.date(),
+    avgWeightInGrams: decimalNumber,
     sampleSize: z.number().positive("Please provide sample size"),
 });
 
