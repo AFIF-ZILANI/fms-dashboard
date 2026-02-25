fe6616ed9749a00b8e58fddf588c046d26773237
AFIF ZILANI <afifzilani4566@gmail.com>
2026-02-25 15:31:33 +0600

    feat: enhance authentication UI with Google sign-in button and update navigation components for user session management

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import SignIn from "@/components/signin-button";
+
 export default function Home() {
-  return (
-  <div className="grid grid-cols-1 md:px-4 lg:px-6">
-    Home Page
-  </div>
-  )
+    return (
+        <div className="grid grid-cols-1 md:px-4 lg:px-6">
+            Home Page
+            <SignIn />
+        </div>
+    );
 }
--- components/navigation/app-sidebar.tsx
+++ components/navigation/app-sidebar.tsx
@@ -24,144 +24,155 @@ import {
     SidebarHeader,
     SidebarRail,
 } from "@/components/ui/sidebar";
+import { useAuthSession } from "@/lib/auth-helper";
+import { usePathname } from "next/navigation";
 
 // This is sample data.
-const data = {
-    user: {
-        name: "afifzilani",
-        email: "afifzilani4566@gmail.com",
-        avatar: "https://github.com/AFIF-ZILANI.png",
-    },
-    teams: [
-        {
-            name: "ZeroD Farms Ltd.",
-            logo: GalleryVerticalEnd,
-            plan: "Enterprise",
-        },
-        {
-            name: "ZeroD Farms Ltd.",
-            logo: AudioWaveform,
-            plan: "Startup",
-        },
-        {
-            name: "ZeroD Ltd.",
-            logo: Command,
-            plan: "Free",
-        },
-    ],
-    navMain: [
-        {
-            title: "Batches",
-            url: "/batches",
-            icon: Layers,
-            isActive: true,
-            items: [
-                {
-                    title: "History",
-                    url: "batc",
-                },
-            ],
-        },
-        {
-            title: "Houses",
-            url: "/houses",
-            icon: House,
-            items: [
-                {
-                    title: "Genesis",
-                    url: "#",
-                },
-                {
-                    title: "Explorer",
-                    url: "#",
-                },
-                {
-                    title: "Quantum",
-                    url: "#",
-                },
-            ],
-        },
-        {
-            title: "Suppliers",
-            url: "/suppliers",
-            icon: Truck,
-            items: [
-                {
-                    title: "Introduction",
-                    url: "#",
-                },
-                {
-                    title: "Get Started",
-                    url: "#",
-                },
-                {
-                    title: "Tutorials",
-                    url: "#",
-                },
-                {
-                    title: "Changelog",
-                    url: "#",
-                },
-            ],
-        },
-        {
-            title: "Inventory",
-            url: "/inventory",
-            icon: Warehouse,
-            items: [
-                {
-                    title: "New Item",
-                    url: "/inventory/new/item",
-                },
-                {
-                    title: "New Medicine",
-                    url: "/inventory/new/medicine",
-                },
-            ],
-        },
-        {
-            title: "Payments",
-            url: "/payments",
-            icon: Wallet,
-            items: [
-                {
-                    title: "General",
-                    url: "/payments",
-                },
-            ],
-        },
-        {
-            title: "Purchases",
-            url: "/purchases",
-            icon: ShoppingBag,
-            items: [
-                {
-                    title: "New Item",
-                    url: "/purchases/new/item",
-                },
-            ],
-        },
-    ],
-    // projects: [
-    //     {
-    //         name: "Design Engineering",
-    //         url: "#",
-    //         icon: Frame,
-    //     },
-    //     {
-    //         name: "Sales & Marketing",
-    //         url: "#",
-    //         icon: PieChart,
-    //     },
-    //     {
-    //         name: "Travel",
-    //         url: "#",
-    //         icon: Map,
-    //     },
-    // ],
-};
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+    const pathname = usePathname();
+    const { user } = useAuthSession();
+
+    console.log(user);
+
+    const data = {
+        user: {
+            name: user?.name,
+            email: user?.email,
+            avatar: user?.avatar,
+        },
+        teams: [
+            {
+                name: "ZeroD Farms Ltd.",
+                logo: GalleryVerticalEnd,
+                plan: "Enterprise",
+            },
+            {
+                name: "ZeroD Farms Ltd.",
+                logo: AudioWaveform,
+                plan: "Startup",
+            },
+            {
+                name: "ZeroD Ltd.",
+                logo: Command,
+                plan: "Free",
+            },
+        ],
+        navMain: [
+            {
+                title: "Batches",
+                url: "/batches",
+                icon: Layers,
+                isActive: true,
+                items: [
+                    {
+                        title: "History",
+                        url: "batc",
+                    },
+                ],
+            },
+            {
+                title: "Houses",
+                url: "/houses",
+                icon: House,
+                items: [
+                    {
+                        title: "Genesis",
+                        url: "#",
+                    },
+                    {
+                        title: "Explorer",
+                        url: "#",
+                    },
+                    {
+                        title: "Quantum",
+                        url: "#",
+                    },
+                ],
+            },
+            {
+                title: "Suppliers",
+                url: "/suppliers",
+                icon: Truck,
+                items: [
+                    {
+                        title: "Introduction",
+                        url: "#",
+                    },
+                    {
+                        title: "Get Started",
+                        url: "#",
+                    },
+                    {
+                        title: "Tutorials",
+                        url: "#",
+                    },
+                    {
+                        title: "Changelog",
+                        url: "#",
+                    },
+                ],
+            },
+            {
+                title: "Inventory",
+                url: "/inventory",
+                icon: Warehouse,
+                items: [
+                    {
+                        title: "New Item",
+                        url: "/inventory/new/item",
+                    },
+                    {
+                        title: "New Medicine",
+                        url: "/inventory/new/medicine",
+                    },
+                ],
+            },
+            {
+                title: "Payments",
+                url: "/payments",
+                icon: Wallet,
+                items: [
+                    {
+                        title: "General",
+                        url: "/payments",
+                    },
+                ],
+            },
+            {
+                title: "Purchases",
+                url: "/purchases",
+                icon: ShoppingBag,
+                items: [
+                    {
+                        title: "New Item",
+                        url: "/purchases/new/item",
+                    },
+                ],
+            },
+        ],
+        // projects: [
+        //     {
+        //         name: "Design Engineering",
+        //         url: "#",
+        //         icon: Frame,
+        //     },
+        //     {
+        //         name: "Sales & Marketing",
+        //         url: "#",
+        //         icon: PieChart,
+        //     },
+        //     {
+        //         name: "Travel",
+        //         url: "#",
+        //         icon: Map,
+        //     },
+        // ],
+    };
+    if (pathname.startsWith("/login")) {
+        return null;
+    }
+
     return (
         <Sidebar collapsible="icon" {...props}>
             <SidebarHeader>
