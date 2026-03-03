40795d1a8f67f565cf171742a4bdfdbd512642bd
AFIF ZILANI <afifzilani4566@gmail.com>
2026-03-04 01:05:11 +0600

    feat: Integrate Vercel Speed Insights for performance monitoring.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,48 +3,49 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AppSidebar } from "@/components/navigation/app-sidebar";
 import AppTopbar from "@/components/navigation/app-topbar";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import ReactQueryClientProvider from "@/components/providers/query-client-provider";
 import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Grappine Dashboard | AI Farm Management",
-  description:
-    "A smart dashboard powered by Grappine AI to monitor, automate, and manage your poultry farm. Real time insights, digital record keeping, and better decision making in one place.",
+    title: "Grappine Dashboard | AI Farm Management",
+    description:
+        "A smart dashboard powered by Grappine AI to monitor, automate, and manage your poultry farm. Real time insights, digital record keeping, and better decision making in one place.",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning={true}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ReactQueryClientProvider>
-          <SidebarProvider>
-            <AppSidebar />
-            <SidebarInset>
-              <AppTopbar />
-              {children}
-            </SidebarInset>
-          </SidebarProvider>
-          <Toaster />
-        </ReactQueryClientProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning={true}>
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                <ReactQueryClientProvider>
+                    <SidebarProvider>
+                        <AppSidebar />
+                        <SidebarInset>
+                            <AppTopbar />
+                            {children}
+                            <SpeedInsights />
+                        </SidebarInset>
+                    </SidebarProvider>
+                    <Toaster />
+                </ReactQueryClientProvider>
+            </body>
+        </html>
+    );
 }
