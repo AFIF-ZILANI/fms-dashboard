86a0f2947939c41298f01cc85851011bc662e762
AFIF ZILANI <afifzilani4566@gmail.com>
2026-03-04 11:24:16 +0600

    feat: validate DATABASE_URL environment variable presence

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,10 @@
 import { PrismaClient } from "../app/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not defined");
+}
+
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient;
 };
