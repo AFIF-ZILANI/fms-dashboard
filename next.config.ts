8c7e88fb769bc196fc8e420daacc74b944783ce5
AFIF ZILANI <afifzilani4566@gmail.com>
2026-03-07 00:33:50 +0600

    fix: nextjs confilg file

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,21 @@
 import type { NextConfig } from "next";
 
+const cloudName = process.env.CLOUDINARY_CLOUD_NAME;
+
+if (!cloudName) {
+  throw new Error("CLOUDINARY_CLOUD_NAME is missing");
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   images: {
-    domains: ["res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: `/${cloudName}/**`,
+      },
+    ],
   },
 };
 
